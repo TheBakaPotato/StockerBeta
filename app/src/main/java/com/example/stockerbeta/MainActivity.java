03768661b9f9caf1dd51d3868dac1b4694535d6d
TheBakaPotato <TheBakaPotato@thebakapotato.com>
2022-01-13 23:07:39 -0300

    First layouts

--- app/src/main/java/com/example/stockerbeta/MainActivity.java
+++ app/src/main/java/com/example/stockerbeta/MainActivity.java
@@ -5,7 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//    objeto
+//      caracteristicas
+//      quantidade
+//      foto
+//      categoria
+//    estoque total
+//      qual produto
+//      quantas unidades tem
+//      qual o minimo
+//      onde está
+//    local
+//      nome
+//    estoque local
+//      local
+//      produto
+//      quantidade
+//    registro de estoque
+//      qual produto
+//      quantas unidades foram operadas
+//      quando foi realizado
+//      onde foi operado
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
